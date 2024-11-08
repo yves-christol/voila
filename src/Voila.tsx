1140558667f8f140ab668ec4c3b32cd9fa7d24fe
Yves Christol <yves.christol@gmail.com>
2024-11-08 10:10:23 +0100

    First

---
+++ src/Voila.tsx
@@ -0,0 +1,249 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
+  authDomain: "voila-b905e.firebaseapp.com",
+  projectId: "voila-b905e",
+  storageBucket: "voila-b905e.appspot.com",
+  messagingSenderId: "78788802866",
+  appId: "1:78788802866:web:eea76db31a06a8b43d7f2b",
+  measurementId: "G-6Y9XVJNEZJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const Voila = () => {
+  return (
+    <div class='margin: auto'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="white" id="Calque_1" data-name="Calque 1" viewBox="0 0 892 502" width="100%" height="auto">
+        <defs>
+          <filter id="f1" x="0" y="0">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="9" />
+          </filter>
+        </defs>
+        <g filter="url(#f1)">
+          <polygon 
+            class="cls-V1" 
+            points="248.78 339.52 215.01 339.52 167 171.88 211.01 171.88 248.78 339.52"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0.5;0;1;1;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;1;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+          <polygon 
+            class="cls-V2" 
+            points="246.73 339.52 280.5 339.52 328.51 171.88 284.49 171.88 246.73 339.52"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;1;0.5;1;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;1;0.5;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+          <path 
+            class="cls-o" 
+            d="M388.18,309.18c-18.78,0-27.69-9.48-27.69-29.44,0-20.48,8.91-29.92,27.69-29.92,19.48,0,28.63,9.48,28.63,29.92C416.81,299.7,407.68,309.18,388.18,309.18Zm.43-92.86c-35.36,0-65,24-65,63.23,0,39,29.61,62.75,65,62.75s64.95-23.74,64.95-62.75C453.56,240.3,424,216.32,388.61,216.32Z"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;1;1;0.5;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;0.5;1;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </path>
+          <polygon 
+            class="cls-i" 
+            points="473.62 230.34 510.63 216.72 510.63 339.94 473.62 339.94 473.62 230.34"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;0.5;1;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;0.5;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+          <path 
+            class="cls-dot" 
+            d="M491.78,202.93c-13.7,0-20.21-6.92-20.21-21.48,0-14.92,6.51-21.84,20.21-21.84,14.21,0,20.88,6.92,20.88,21.84C512.66,196,506,202.93,491.78,202.93Z"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="20s"
+              values="0;0;0;0;0;0;0;0.5;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;0.5;1;1;0.5;0.4;0.3;0.2;0.1;0;0;0;0;0;0;0;0;0;0;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </path>
+          <rect 
+            class="cls-l" 
+            x="537.03" y="171.88" width="36.8" height="167.64"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;0.5;0.5;0.5;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;0.5;1;1;1;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </rect>
+          <path 
+            class="cls-a" 
+            d="M660.58,308.82c-18.78,0-27.7-9.49-27.7-29.45,0-20.47,8.92-29.92,27.7-29.92,19.48,0,28.63,9.45,28.63,29.92C689.21,299.33,680.06,308.82,660.58,308.82ZM688,218.32v28.23c-7.08-20-20.72-30.66-41.41-30.66-33.33,0-50.8,25.3-50.8,63.51,0,37.72,18.44,63,53.24,63,19.47,0,32.15-10.47,39-29.69V340h37V218.32Z"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;1;0.5;0.5;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </path>
+          <polygon 
+            class="cls-hat" 
+            points="662.86 199.64 627.52 174.16 660.15 161.2 677.17 195.59 662.86 199.64"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;1;0.5;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;1;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+        </g>
+        <g>
+        <polygon 
+            class="cls-V1" 
+            points="248.78 339.52 215.01 339.52 167 171.88 211.01 171.88 248.78 339.52"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0.5;0;1;1;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;1;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+          <polygon 
+            class="cls-V2" 
+            points="246.73 339.52 280.5 339.52 328.51 171.88 284.49 171.88 246.73 339.52"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;1;0.5;1;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;1;0.5;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+          <path 
+            class="cls-o" 
+            d="M388.18,309.18c-18.78,0-27.69-9.48-27.69-29.44,0-20.48,8.91-29.92,27.69-29.92,19.48,0,28.63,9.48,28.63,29.92C416.81,299.7,407.68,309.18,388.18,309.18Zm.43-92.86c-35.36,0-65,24-65,63.23,0,39,29.61,62.75,65,62.75s64.95-23.74,64.95-62.75C453.56,240.3,424,216.32,388.61,216.32Z"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;1;1;0.5;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;0.5;1;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </path>
+          <polygon 
+            class="cls-i" 
+            points="473.62 230.34 510.63 216.72 510.63 339.94 473.62 339.94 473.62 230.34"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;0.5;1;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;0.5;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+          <path 
+            class="cls-dot" 
+            d="M491.78,202.93c-13.7,0-20.21-6.92-20.21-21.48,0-14.92,6.51-21.84,20.21-21.84,14.21,0,20.88,6.92,20.88,21.84C512.66,196,506,202.93,491.78,202.93Z"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="20s"
+              values="0;0;0;0;0;0;0;0.5;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;0.5;1;1;0.5;0.4;0.3;0.2;0.1;0;0;0;0;0;0;0;0;0;0;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </path>
+          <rect 
+            class="cls-l" 
+            x="537.03" y="171.88" width="36.8" height="167.64"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;0.5;0.5;0.5;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;0.5;1;1;1;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </rect>
+          <path 
+            class="cls-a" 
+            d="M660.58,308.82c-18.78,0-27.7-9.49-27.7-29.45,0-20.47,8.92-29.92,27.7-29.92,19.48,0,28.63,9.45,28.63,29.92C689.21,299.33,680.06,308.82,660.58,308.82ZM688,218.32v28.23c-7.08-20-20.72-30.66-41.41-30.66-33.33,0-50.8,25.3-50.8,63.51,0,37.72,18.44,63,53.24,63,19.47,0,32.15-10.47,39-29.69V340h37V218.32Z"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;1;0.5;0.5;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;0;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </path>
+          <polygon 
+            class="cls-hat" 
+            points="662.86 199.64 627.52 174.16 660.15 161.2 677.17 195.59 662.86 199.64"
+          >
+            <animate
+              attributeName="opacity"
+              begin="0s"
+              dur="10s"
+              values="0;0;0;1;0.5;1;1;1;1;1;0.5;1;1;1;0.5;1;1;1;1;1;1;1;1;0.5;1;0"
+              repeatCount={1} 
+              fill="freeze"
+            />
+          </polygon>
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+export default Voila
